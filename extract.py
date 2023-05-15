367add72ed4afdad2f02fe007b33b31bcd213a7e
Wasss123 <wassim.khelil.edu@gmail.com>
2023-05-15 21:25:04 +0200

    4

--- extract.py
+++ extract.py
@@ -2,15 +2,16 @@ import os
 import csv
 import json
 
+
 def extract_drugs(filename):
-    #extraction des medicament du fichier drug csv
-    #Stocker les données dans un fichier json
+    # extraction des medicament du fichier drug csv
+    # Stocker les données dans un fichier json
     drugs = {}
-    with open(filename, 'r') as csvfile:
+    with open(filename, "r") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            drug_id = row['atccode']
-            drug_name = row['drug']
+            drug_id = row["atccode"]
+            drug_name = row["drug"]
             drugs[drug_id] = drug_name
     return drugs
 
@@ -19,17 +20,17 @@ def extract_publications(filename):
     # extraction des publication du fichier pubmed csv
     # Stocker les données dans un fichier json
     publications = {}
-    with open(filename, 'r') as csvfile:
+    with open(filename, "r") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            publication_id = row['id']
-            title = row['title']
-            journal = row['journal']
-            date = row['date']
+            publication_id = row["id"]
+            title = row["title"]
+            journal = row["journal"]
+            date = row["date"]
             publications[publication_id] = {
-                'title': title,
-                'journal': journal,
-                'date': date
+                "title": title,
+                "journal": journal,
+                "date": date,
             }
     return publications
 
@@ -38,21 +39,22 @@ def extract_clinical_trials(filename):
     # extraction des medicament du fichier clinal trial csv
     # Stocker les données dans un fichier json
     clinical_trials = {}
-    with open(filename, 'r') as csvfile:
+    with open(filename, "r") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            trial_id = row['id']
-            title = row['scientific_title']
-            journal = row['journal']
-            date = row['date']
+            trial_id = row["id"]
+            title = row["scientific_title"]
+            journal = row["journal"]
+            date = row["date"]
             clinical_trials[trial_id] = {
-                'title': title,
-                'journal': journal,
-                'date': date
+                "title": title,
+                "journal": journal,
+                "date": date,
             }
     return clinical_trials
 
+
 def extract_publications_json(filename):
-    with open(filename, 'r') as mon_fichier:
+    with open(filename, "r") as mon_fichier:
         publications = json.load(mon_fichier)
     return publications
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import extract
-import generate
+import save
 import process
 
-if __name__ == "__main__" :
-    #extraction des données
+if __name__ == "__main__":
+    # extraction des données
     Drugs = extract.extract_drugs("./data/drugs.csv")
     Pubmed = extract.extract_publications("./data/pubmed.csv")
-    Pubmed.pop('\t')
+    Pubmed.pop("\t")
     Clinals = extract.extract_clinical_trials("./data/clinical_trials.csv")
 
-    #Generation du JSON de liaison
+    # Generation du JSON de liaison
     result = {}
     for drug in Drugs:
         result[drug] = {
-            'journal': process.find_in_journal(drug,Drugs,Pubmed,Clinals),
-            'pubmed': process.find_in_source(drug,Drugs, Pubmed),
-            'scientific_title': process.find_in_source(drug,Drugs, Clinals),
+            "journal": process.find_in_journal(drug, Drugs, Pubmed, Clinals),
+            "pubmed": process.find_in_source(drug, Drugs, Pubmed),
+            "scientific_title": process.find_in_source(drug, Drugs, Clinals),
         }
 
     # Stockage du fichier JSON
-    generate.ecrire(result)
+    save.ecrire(result)
